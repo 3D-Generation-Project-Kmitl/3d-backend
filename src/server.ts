5410cdee1bbbd01fe8d9eaeff3f678c53f8960f4
pjpure <pure254326@gmail.com>
2022-12-12 00:55:32 +0700

    fix bug cors

--- src/server.ts
+++ src/server.ts
@@ -19,17 +19,17 @@ declare global {
 
 const server: Application = express();
 
+server.use(cors({
+    origin: true,
+    credentials: true,
+}));
+
 server.set('port', process.env.PORT || 8080);
 
 server.use(express.json());
 server.use(cookieParser());
 server.use(express.urlencoded({ extended: true }));
 server.use('/uploads', express.static('uploads'));
-
-server.use(cors({
-    origin: true,
-    credentials: true,
-}));
 
 server.use('/api/health-check', (req, res) => {
     res.send(`I'm up and running on v1.0.0`);
