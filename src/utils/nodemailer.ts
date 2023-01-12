2e9652986ca6a527d22f592bca174552cf3b9473
pjpure <pure254326@gmail.com>
2023-01-12 15:26:34 +0700

    update nodemail

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -1,15 +1,11 @@
 import emailConfig from '../configs/email';
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const { env } = process;
 const transporter = nodemailer.createTransport(emailConfig);
 
 export const sendOTP = async (email: string, otp: string) => {
     const mailOptions = {
-        from: env.EMAIL_USER,
+        from: emailConfig.auth.user,
         to: email,
         subject: 'OTP for reset password',
         text: `Your OTP is ${otp}`,
