4f5380d3f2c8c575b1d811065d27cb5d9cfb8a32
pjpure <pure254326@gmail.com>
2023-03-12 16:06:33 +0700

    update identity admin api

--- src/routes/identity.ts
+++ src/routes/identity.ts
@@ -8,7 +8,8 @@ const router = Router();
 router.post('/', [verifyToken, upload.fields([{ name: 'cardPicture' }, { name: 'cardFacePicture' }])], identityController.create);
 router.get('/', [verifyToken], identityController.get);
 router.put('/', [verifyToken, upload.fields([{ name: 'cardPicture' }, { name: 'cardFacePicture' }])], identityController.update);
-router.put('/admin', [verifyToken], identityController.adminUpdate);
+router.get('/admin', [verifyToken, permit('ADMIN')], identityController.adminGetPendingIdentities);
+router.patch('/admin', [verifyToken, permit('ADMIN')], identityController.adminUpdate);
 
 
 export default router;
